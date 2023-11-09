8787bc1bfe44edd9b879d34d33f041f03eb2d82a
madddtone <madddtone@gmail.com>
2023-11-09 20:13:52 +0700

    add solutions eloquent

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -15,14 +16,21 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+       $users = User::whereNotNull('email_verified_at')
+            ->orderByDesc('created_at')
+            ->limit(3)
+            ->get();
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::find($userId);
+
+        if (!$user) {
+            abort(404, 'User not found');
+        }
 
         return view('users.show', compact('user'));
     }
@@ -31,16 +39,28 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrNew(['name' => $name, 'email' => $email]);
+
+        if (!$user->exists) {
+            $user->password = bcrypt(Str::random(10));
+            $user->save();
+        }
 
         return view('users.show', compact('user'));
     }
 
     public function check_update($name, $email)
     {
-        // TASK: find a user by $name and update it with $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::firstOrNew(['name' => $name]);
+        $user->email = $email;
+
+        // If the user is new (not yet saved in the database), set a random password and save
+        if (!$user->exists) {
+            $user->password = bcrypt(Str::random(10));
+            $user->save();
+        } else {
+            $user->save(); // If the user exists, update the email
+        }
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +72,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        User::destroy($request->users);
 
         return redirect('/')->with('success', 'Users deleted');
     }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,8 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Observers\ProjectObserver;
 
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::observe(ProjectObserver::class);
+    }
 }
